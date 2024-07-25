a375e570bbb6e52eb297cb65279f4894a9b46cb3
gdohmeier <gdohmeier@gmail.com>
2024-07-25 16:53:03 -0400

    GamesPagePreview chg to GamesPostPreview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,6 +3,7 @@ import uploadcare from 'decap-cms-media-library-uploadcare'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+import GamesPostPreview from './preview-templates/GamesPostPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
@@ -10,3 +11,5 @@ CMS.registerMediaLibrary(uploadcare)
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('games', GamesPostPreview)
+
