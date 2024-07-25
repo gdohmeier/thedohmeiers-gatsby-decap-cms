04a601943bdcab9ec4197d5e7f9b7d9ccb88015a
gdohmeier <gdohmeier@gmail.com>
2024-07-25 16:42:53 -0400

    remove import GamesPreviewPage

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,12 +3,10 @@ import uploadcare from 'decap-cms-media-library-uploadcare'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import GamesPagePreview from './preview-templates/GamesPagePreview-old'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('games', GamesPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
